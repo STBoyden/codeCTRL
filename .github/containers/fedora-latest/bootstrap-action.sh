05f3f346e6f743ac81e2f36128a8fe2357a4ab90
STBoyden <sam@stboyden.com>
2023-07-27 07:19:36 +0100

    Revise dependencies for Linux. Fixes #6.

--- .github/containers/fedora-latest/bootstrap-action.sh
+++ .github/containers/fedora-latest/bootstrap-action.sh
@@ -11,19 +11,10 @@ case $ID in
         export DEBIAN_FRONTEND=noninteractive
 
         apt update -y
-        pixbuf_dev=""
-        case "$VERSION_CODENAME" in
-            "focal" | "buster")
-                pixbuf_dev="libgdk-pixbuf2.0-dev"
-                ;;
-            *)
-                pixbuf_dev="libgdk-pixbuf-2.0-dev"
-                ;;
-        esac
-        apt install build-essential clang cmake libglib2.0-dev libpango1.0-dev "$pixbuf_dev" libatk1.0-dev libgtk-3-dev libxcb-shape0-dev libxcb-xfixes0-dev curl libc6-dev libsqlite3-dev git -y
+        apt install build-essential libfreetype-dev libfontconfig-dev
         ;;
     "fedora" | "rocky")
-        packages=(gobject-introspection-devel cairo-devel atk-devel pango-devel gdk-pixbuf2-devel gtk3-devel clang curl cmake git)
+        packages=(freetype-devel expat-devel fontconfig-devel)
 
         dnf="$(command -v dnf)"
 
@@ -40,7 +31,7 @@ case $ID in
         if [[ "$ID" == "rocky" && $(rpm -E %rhel) -ge 9 ]]; then
             $dnf --enablerepo=crb --allowerasing install "${packages[@]}" -y
         else
-            $dnf install "${packages[@]}" -y
+            $dnf install "${packages[@]}" g++ -y
         fi
         ;;
     "centos")
@@ -48,12 +39,7 @@ case $ID in
         yum update -y
         yum groupinstall "Development Tools" -y
 
-        yum install gobject-introspection-devel cairo-devel atk-devel pango-devel gdk-pixbuf2-devel gtk3-devel clang curl cmake3 git -y
+        yum install freetype-devel expat-devel fontconfig-devel cmake3 -y
         ln -s /usr/bin/cmake3 /usr/bin/cmake
         ;;
 esac
-
-if [[ -n $(command -v rustup 2>/dev/null ) ]]; then
-    echo -e "\nInstalling wasm32-unknown-unknown Rust target..."
-    rustup target install wasm32-unknown-unknown
-fi
--- bootstrap-build.sh
+++ bootstrap-build.sh
@@ -2,40 +2,55 @@
 
 source /etc/os-release
 
+if [ $(id -u) -ne 0 ]; then
+    echo "Please run this script as root"
+    exit 1
+fi
+
 echo -e "\nInstalling dependencies for your distro...\n"
 
 case $ID in
     "debian" | "ubuntu" | "elementary")
-        sudo apt install build-essential gcc clang -y
-        pixbuf_dev=""
-        case "$VERSION_CODENAME" in
-            "focal" | "buster")
-                pixbuf_dev="libgdk-pixbuf2.0-dev"
-            ;;
-            *)
-                pixbuf_dev="libgdk-pixbuf-2.0-dev"
-            ;;
-        esac
-        sudo apt install libglib2.0-dev libpango1.0-dev "$pixbuf_dev" libatk1.0-dev libgtk-3-dev libxcb-shape0-dev libxcb-xfixes0-dev curl -y
-    ;;
-    "fedora")
-        dnf="$(which dnf)"
+        export DEBIAN_FRONTEND=noninteractive
+
+        apt update -y
+        apt install build-essential libfreetype-dev libfontconfig-dev
+        ;;
+    "fedora" | "rocky")
+        packages=(freetype-devel expat-devel fontconfig-devel)
+
+        dnf="$(command -v dnf)"
+
+        $dnf update -y
 
         if [ -f "$(command -v dnf5)" ]; then
-            dnf="$(which dnf5)"
-            sudo $dnf group install development-tools -y
+            dnf="$(command -v dnf5)"
+            sudo "$dnf" group install development-tools -y
         else
-            sudo $dnf groupinstall "Development Tools" -y
+            $dnf groupinstall "Development Tools" -y
         fi
 
-        sudo $dnf install gobject-introspection-devel cairo-devel atk-devel pango-devel gdk-pixbuf2-devel gtk3-devel clang curl cmake -y
-    ;;
+
+        if [[ "$ID" == "rocky" && $(rpm -E %rhel) -ge 9 ]]; then
+            $dnf --enablerepo=crb --allowerasing install "${packages[@]}" -y
+        else
+            $dnf install "${packages[@]}" g++ -y
+        fi
+        ;;
+    "centos")
+        yum install epel-release -y
+        yum update -y
+        yum groupinstall "Development Tools" -y
+
+        yum install freetype-devel expat-devel fontconfig-devel cmake3 -y
+        ln -s /usr/bin/cmake3 /usr/bin/cmake
+        ;;
     "arch")
         # TODO: Add Arch Linux packages
-    ;;
+        ;;
     *)
         echo -e "Unknown distribution, please manually find and install the relevant packages for your distro.\n"
-    ;;
+        ;;
 esac
 
 if [[ -z $(which rustup 2>/dev/null) ]]; then
@@ -43,11 +58,5 @@ if [[ -z $(which rustup 2>/dev/null) ]]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- --default-toolchain nightly -y
     source $HOME/.cargo/env
 fi
-
-echo -e "\nInstalling wasm32-unknown-unknown Rust target..."
-rustup target install wasm32-unknown-unknown
-
-echo -e "\nInstalling trunk..."
-cargo install trunk
 
 echo -e "\nIf you're inside a toolbox or distrobox, you need to install the necessary display drivers for your GPU *inside* the container otherwise you will run into OpenGL issues."
